d1535253ed7eeb44d47a2a76fce77ed1f1173bf0
whispering233 <1767779281@qq.com>
2024-07-20 04:25:31 +0800

    增加洗牌算法

--- poker/src/texas_hold.clj
+++ poker/src/texas_hold.clj
@@ -1,8 +1,16 @@
 (ns texas-hold)
 
 (defn decompose [coli] (map-indexed (fn [index value] (list index value)) coli))
-(def poker (for [suit (decompose (list "♣" "♠" "♦" "♥"))
-                 number (decompose (list "A" "2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K"))]
-             ((fn [[a b] [c d]] (list a c (str b d))) suit number)))
+(def poker (vec (for [suit (decompose (list "♣" "♠" "♦" "♥"))
+                      number (decompose (list "A" "2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K"))]
+                  [(first suit) (first number) (str (second suit) (second number))])))
+(defn shuffle-poker [poker]
+  (if (empty? poker)
+    []
+    (let [idx (rand-int (count poker))]
+      (concat [(nth poker idx)]
+              (shuffle-poker (concat (subvec (vec poker) 0 idx)
+                                     (subvec (vec poker) (inc idx))))))))
+(defn get-poker [] (shuffle-poker poker))
 
 
