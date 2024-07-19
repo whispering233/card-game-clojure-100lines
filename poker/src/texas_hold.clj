c4efb1a8d63ac3a9fcc271bc6053bc1199786c33
whispering233 <1767779281@qq.com>
2024-07-20 01:51:56 +0800

    修正数字牌型

--- poker/src/texas_hold.clj
+++ poker/src/texas_hold.clj
@@ -2,5 +2,7 @@
 
 (defn decompose [coli] (map-indexed (fn [index value] (list index value)) coli))
 (def poker (for [suit (decompose (list "♣" "♠" "♦" "♥"))
-                 number (decompose (list "A" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K"))]
+                 number (decompose (list "A" "2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K"))]
              ((fn [[a b] [c d]] (list a c (str b d))) suit number)))
+
+
