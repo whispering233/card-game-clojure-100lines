61aa01f1c06b1a7530b2becd56b7ead885b2c7f1
whispering233 <1767779281@qq.com>
2024-07-21 04:23:02 +0800

    简化代码

--- poker/src/texas_hold.clj
+++ poker/src/texas_hold.clj
@@ -1,16 +1,22 @@
 (ns texas-hold)
 
-(defn decompose [coli] (map-indexed (fn [index value] (list index value)) coli))
-(def poker (vec (for [suit (decompose (list "♣" "♠" "♦" "♥"))
-                      number (decompose (list "A" "2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K"))]
-                  [(first suit) (first number) (str (second suit) (second number))])))
-(defn shuffle-poker [poker]
-  (if (empty? poker)
-    []
-    (let [idx (rand-int (count poker))]
-      (concat [(nth poker idx)]
-              (shuffle-poker (concat (subvec (vec poker) 0 idx)
-                                     (subvec (vec poker) (inc idx))))))))
-(defn get-poker [] (shuffle-poker poker))
+(def player ["Mine" "Player1" "Player2" "Player3" "Player4" "Player5" "Player6" "Player7" "Player8"])
+(def seat ["SB" "BB" "UTG" "UTG+1" "UTG+2" "LJ" "HJ" "CO" "BTN"])
+(def suit ["♣" "♠" "♦" "♥"])
+(def rank ["2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K" "A"])
+(defn get-deck [] (shuffle (vec (for [a suit b rank] (str a b)))))
+(defn deal-hands [shuffle-deck ro-seat]
+  (interleave
+    (apply concat (repeat 2 (partition 2 (interleave ro-seat player))))
+    (partition 2 (take (* 2 (count player)) shuffle-deck))))
+(defn deal-public [shuffle-deck] (take 5 (drop (* 2 (count player)) shuffle-deck)))
+(defn start []
+  (let [deck (get-deck) hands (deal-hands deck seat) public (deal-public deck)]
+    (println "Mine hands is " (nth hands (+ 1 (* 2 (.indexOf player "Mine")))))
+    (println "Public " public)))
 
 
+(defn handle-input []
+  (println "Please enter something:")
+  (let [input (read-line)]
+    (println "You entered:" input)))
