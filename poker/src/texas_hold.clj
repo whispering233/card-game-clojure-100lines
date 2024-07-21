f40bc80deceda5aa0967e708d850ee211fcd9ef8
whispering233 <1767779281@qq.com>
2024-07-22 01:38:32 +0800

    简化代码

--- poker/src/texas_hold.clj
+++ poker/src/texas_hold.clj
@@ -5,16 +5,11 @@
 (def suit ["♣" "♠" "♦" "♥"])
 (def rank ["2" "3" "4" "5" "6" "7" "8" "9" "10" "J" "Q" "K" "A"])
 (defn get-deck [] (shuffle (vec (for [a suit b rank] (str a b)))))
-(defn deal-hands [shuffle-deck]
-  (apply hash-map (interleave seat
-                              (partition 2 (take (* 2 (count player)) shuffle-deck)))))
-(defn deal-public [shuffle-deck] (take 5 (drop (* 2 (count player)) shuffle-deck)))
-(defn bind-player [ro-seat] (apply hash-map (interleave ro-seat player)))
 (defn rotate-seat [seat] (concat (take-last 1 seat) (drop-last 1 seat)))
 (defn start [deck ro-seat]
-  (let [seat-player (bind-player ro-seat)
-        hands (deal-hands deck)
-        public (deal-public deck)]
+  (let [seat-player (apply hash-map (interleave ro-seat player))
+        hands (apply hash-map (interleave seat (partition 2 (take (* 2 (count player)) deck))))
+        public (take 5 (drop (* 2 (count player)) deck))]
     (doseq [speaker seat] (println speaker (seat-player speaker) (hands speaker)))
     (println public))
   (let [continue (read-line)]
